843d0d1a017af8ab5192c012fa850ae94f7f4c43
chandana <chandanaganji09@gmail.com>
2025-06-06 07:32:19 +0530

    Initial commit for Educase assignment

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Profile from './components/Profile';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
